8043a514fabd43b1164e168aaef7ed002e125c2b
PHOOMPHAT-Git <phomphat2011@gmail.com>
2026-03-04 16:40:25 +0700

    first commit

---
+++ Garden Horizons/Main.lua
@@ -0,0 +1,36 @@
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+
+local Window = Fluent:CreateWindow({
+	Title = "Garden Horizons",
+	SubTitle = "PrimeXploit",
+	TabWidth = 160,
+	Size = UDim2.fromOffset(580, 460),
+	Acrylic = false,
+	Theme = "Dark",
+	MinimizeKey = Enum.KeyCode.LeftControl
+})
+
+local Tabs = {
+	Main = Window:AddTab({ Title = "Main", Icon = "home" }),
+	Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
+
+local Options = Fluent.Options
+
+-- Main
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PrimeXploit/Script/refs/heads/main/Garden%20Horizons/Tabs/Main.Lua"))().Init(Tabs.Main, Options, Fluent)
+
+-- Settings
+local SettingsManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/PrimeXploit/Script/refs/heads/main/Garden%20Horizons/Tabs/Settings.lua"))()
+
+SettingsManager:SetLibrary(Fluent)
+SettingsManager:SetFolder("PrimeXploitSettings")
+SettingsManager:BuildInterfaceSection(Tabs.Settings)
+
+Window:SelectTab(1)
+
+Fluent:Notify({
+	Title = "PrimeXploit",
+	Content = "Script loaded successfully.",
+	Duration = 5
+})
