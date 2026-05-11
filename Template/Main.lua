0c07a4c7c2577a90e3ef8fc215bae00c78b1b52b
PrimeXploit <forgivetomyct@gmail.com>
2026-05-11 16:13:47 +0700

    Update Main.lua

--- Template/Main.lua
+++ Template/Main.lua
@@ -18,10 +18,10 @@ local Tabs = {
 local Options = Fluent.Options
 
 -- Main
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PrimeXploit/Script/refs/heads/main/Garden%20Horizons/Tabs/Main.Lua"))().Init(Tabs.Main, Options, Fluent)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PrimeXploit/Script/refs/heads/main/Template/Tabs/Main.Lua"))().Init(Tabs.Main, Options, Fluent)
 
 -- Settings
-local SettingsManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/PrimeXploit/Script/refs/heads/main/Garden%20Horizons/Tabs/Settings.lua"))()
+local SettingsManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/PrimeXploit/Script/refs/heads/main/Template/Tabs/Settings.lua"))()
 
 SettingsManager:SetLibrary(Fluent)
 SettingsManager:SetFolder("PrimeXploitSettings")
